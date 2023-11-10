c31891e68f1ff19b5687deb861cb40a3ce53648f
Arron Abay <arronabay@yahoo.com>
2023-11-10 10:44:01 -0800

    yes

--- ticketing-services/auth/src/routes/signup.ts
+++ ticketing-services/auth/src/routes/signup.ts
@@ -1,9 +1,31 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.get('/api/users/signup', (req, res) => {
-    res.send('hi there')
-})
+router.post('/api/users/signup', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email nust be valid'),
+        body('password')
+            .trim()
+            .isLength({min: 4 , max: 20})
+            .withMessage('password must be valid')
+    ], 
+    (req: Request, res: Response) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            console.log('incorrect values')
+            res.status(400).send({message: 'invalid email or password', errors: errors.array()});
+
+        }
+        else {
+            console.log('creating user now')
+            res.status(200).send('user created now')
+        }
+    }
+);
 
 export {router as signupRouter};
