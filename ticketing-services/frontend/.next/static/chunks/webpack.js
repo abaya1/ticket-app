01c6193fc9e867235b26aba7c51f0c9c7e609890
Arron Abay <arronabay@yahoo.com>
2024-08-17 16:51:23 -0700

    consolidated code into common

--- ticketing-services/frontend/.next/static/chunks/webpack.js
+++ ticketing-services/frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3f1ed860f0e4d12"; }
+/******/ 		__webpack_require__.h = function() { return "69ac644887a5e60b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ ticketing-services/frontend/components/loginBox.js
@@ -0,0 +1,5 @@
+export default ({variation}) => {
+
+    return <div></div>
+
+}
